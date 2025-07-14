d6d6c5571612d929d673de53ec0b499ed85f3392
AeoxHub <163515239+okyimfine@users.noreply.github.com>
2025-07-14 12:42:02 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'backend/goofle-465903-c15c0dc916d5.json',
+  keyFile: 'backend/goofle-465903-c215f9a69e66.json',
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
